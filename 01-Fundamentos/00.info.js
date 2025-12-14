ca048723a6e0a5f650ecef6b91acf7d40de5ee5f
Sebas <sebaslopezlondo@gmail.com>
2025-12-14 17:15:32 -0500

    new exercises in codewars

--- 01-Fundamentos/00.info.js
+++ 01-Fundamentos/00.info.js
@@ -1,6 +1,7 @@
 /*
  * Javascript: No es Java, no confundir.
  * Lenguaje de programación de alto nivel.
+ * Lenguajes interpretado: El código es traducido a medida que se va ejecutando el programa
  */
 
 /*
